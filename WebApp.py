2ff543c44cf4da06d016ec5550b00ff16e8c633e
amws <siddiquimaalik@gmail.com>
2024-01-29 16:24:23 -0500

    Setting up .env

--- WebApp.py
+++ WebApp.py
@@ -1,7 +1,15 @@
 import requests
+import openai
+from dotenv import load_dotenv
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route("/")
 def index():
