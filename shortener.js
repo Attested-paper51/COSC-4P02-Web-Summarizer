ecc87e40a174bc4695c0880fc41981aa6a3b6b25
Amani A <anderson.amani13@gmail.com>
2024-01-30 17:58:42 -0500

    Shortener.js WIP

--- shortener.js
+++ shortener.js
@@ -12,5 +12,15 @@ function shortenURL(longURL,login,apiKey,func) {
         {
             func(response.data.url);
         }
-    )
+    );
 }
+
+var login = "jakematrix2013";
+var apiKey = "45887b601d56d78743fd009748ed9259b8f3a1b3";
+var longURL = "https://platform.openai.com/docs/guides/text-generation";
+
+shortenURL(longURL,login,apiKey,
+    function(short_url){
+        console.log(short_url);
+
+});
