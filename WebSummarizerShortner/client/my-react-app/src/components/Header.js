681571e2009871afa7f52966da805caba56e809c
anjali <anjalisabu.jp@gmail.com>
2024-02-02 16:31:20 -0500

    Changes to Header.js component and App.css

--- WebSummarizerShortner/client/my-react-app/src/components/Header.js
+++ WebSummarizerShortner/client/my-react-app/src/components/Header.js
@@ -2,38 +2,61 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
+
     // css properties
-    const navStyle = {
-      backgroundColor: '#333',
-      color: 'white',
+    const headerStyle = {
+      backgroundColor: '#374375',
+      color: '#FFFCF5',
       padding: '1em',
+      display: 'flex', display: '-webkit-box',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
       textAlign: 'center',
+      alignItems: 'center',
+      gap: '2em',
+    };
+
+    const navStyle = {
+      // position: 'fixed',
+
+      display: 'flex', display: '-webkit-box',
+      justifyContent: 'space-between',
     };
 
     const ulStyle = {
       listStyleType: 'none',
       margin: 0,
       padding: 0,
-      display: 'flex',
-      justifyContent: 'space-around',
+      display: 'flex', display: '-webkit-box',
+      justifyContent: 'space-evenly',
+      height: '100%',
+      flexDirection: 'column',
+      alignItems: 'center',
     };
 
     const liStyle = {
-      display: 'inline-block',
-      marginRight: '1em',
+      // display: 'inline-block',
+      // marginRight: '1em',
     };
 
     return (
-      <header style={navStyle}>
-        <nav>
-          <div>
-            <ul style={ulStyle}>
-              <li style={liStyle}><a href="#">Home</a></li>
-              <li style={liStyle}><a href="#">About</a></li>
-              <li style={liStyle}><a href="#">Contact</a></li>
-            </ul>
-          </div>
+      <header style={headerStyle}>
+
+        <div className='div1'> logo </div>
+
+        <div> shortify </div>
+
+        <nav style={navStyle}>
+          <ul style={ulStyle}>
+            <li style={liStyle}><a href="#">Summarizer</a></li>
+            <li style={liStyle}><a href="#">URL Shortener</a></li>
+          </ul>
         </nav>
+
+        <div> Mode </div>
+
+        <div> Account </div>
+
       </header>
     );
   }
