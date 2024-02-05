48731ced7b555dd7e2975f46c0fd7040cc7dc939
Marium <marium.nur2003@gmail.com>
2024-02-04 19:22:08 -0500

    landing page

--- WebSummarizerShortner/client/my-react-app/src/components/Header.js
+++ WebSummarizerShortner/client/my-react-app/src/components/Header.js
@@ -42,53 +42,100 @@
 // export default Header;
 
 
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+// import "./HeaderStyle.css";
+
+// class Header extends Component {
+//   render() {
+//     return(
+//     <div className="index">
+//       <div className="div">
+//         <div className="logo">
+//           <img
+//             className="star"
+//             alt="Star"
+//             src="https://cdn.animaapp.com/projects/65b1a55658133fdd7aa24152/releases/65bbd6bd8eac5fe801568252/img/star-1.svg"
+//           />
+//           <div className="text-wrapper">Logo</div>
+//         </div>
+//         {/* <div className="search-box">
+//           <img
+//             className="img"
+//             alt="Img"
+//             src="https://cdn.animaapp.com/projects/65b1a55658133fdd7aa24152/releases/65bbd6bd8eac5fe801568252/img/--.svg"
+//           />
+//           <p className="search-for-anything">
+//             <span className="span">Search </span>
+//             <span className="span">for anything</span>
+//           </p>
+//         </div> */}
+//         <div className="nav-links">
+//           <div className="text-wrapper-2">Home</div>
+//           <div className="frame">
+//             <div className="text-wrapper-2">Products</div>
+//             <img
+//               className="akar-icons-chevron"
+//               alt="Akar icons chevron"
+//               src="https://cdn.animaapp.com/projects/65b1a55658133fdd7aa24152/releases/65bbd6bd8eac5fe801568252/img/akar-icons-chevron-down.svg"
+//             />
+//           </div>
+//           <div className="text-wrapper-2">About</div>
+//           <div className="text-wrapper-2">Pricing</div>
+//         </div>
+//         <button className="button">
+//           <div className="buy-now">Buy Now</div>
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+// }
+
+// export default Header;
+
+
+import { Component } from "react";
+import { useState } from "react";
 import "./HeaderStyle.css";
 
 class Header extends Component {
   render() {
-    return(
-    <div className="index">
-      <div className="div">
-        <div className="logo">
-          <img
-            className="star"
-            alt="Star"
-            src="https://cdn.animaapp.com/projects/65b1a55658133fdd7aa24152/releases/65bbd6bd8eac5fe801568252/img/star-1.svg"
-          />
-          <div className="text-wrapper">Logo</div>
-        </div>
-        {/* <div className="search-box">
-          <img
-            className="img"
-            alt="Img"
-            src="https://cdn.animaapp.com/projects/65b1a55658133fdd7aa24152/releases/65bbd6bd8eac5fe801568252/img/--.svg"
-          />
-          <p className="search-for-anything">
-            <span className="span">Search </span>
-            <span className="span">for anything</span>
-          </p>
-        </div> */}
-        <div className="nav-links">
-          <div className="text-wrapper-2">Home</div>
-          <div className="frame">
-            <div className="text-wrapper-2">Products</div>
+    return (
+      <section>
+        <nav className='header-nav'>
+          <a href="/#">
             <img
-              className="akar-icons-chevron"
-              alt="Akar icons chevron"
-              src="https://cdn.animaapp.com/projects/65b1a55658133fdd7aa24152/releases/65bbd6bd8eac5fe801568252/img/akar-icons-chevron-down.svg"
+  
+              className="img"
+              alt="logo"
+              src="images/logo.png"
             />
-          </div>
-          <div className="text-wrapper-2">About</div>
-          <div className="text-wrapper-2">Pricing</div>
-        </div>
-        <button className="button">
-          <div className="buy-now">Buy Now</div>
-        </button>
-      </div>
-    </div>
-  );
-};
+          </a>
+          
+          <h1><a href="/#">shortify</a></h1>
+          <h3 className="nav-text-sum"><a href="/#">Summarizer</a></h3>
+          <h3 className="nav-text-short"><a href="/#">URL Shortner</a></h3>
+          
+          <a href="/#">
+            <img 
+              className="darkmode"
+              alt="darkmode"
+              src="images/darkmode.png"
+            />
+          </a>
+
+          <a href="/#">
+            <img 
+              className="profile"
+              alt="profile"
+              src="images/profile.png"
+            />
+          </a>
+        </nav>
+
+      </section>
+    );
+  };
 }
 
 export default Header;
