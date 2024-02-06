41c25a5df003d41acb6ec325fc318d73f7adcb7a
amws <siddiquimaalik@gmail.com>
2024-02-06 15:43:00 -0500

    url functionality added

--- WebSummarizer/test.py
+++ WebSummarizer/test.py
@@ -1,3 +1,15 @@
-lst = [30, 1, 2, 1, 0]
+import requests
+def processURL(url):
+    endpoint = "https://extractorapi.com/api/v1/extractor"
+    params = {
+        "apikey": "f1fe711b72df1cfa3ad4ff5c1ac8ccc8c8eba7b6",
+        "url": url
+    }
 
-print(lst.count(1))
+    r = requests.get(endpoint, params=params)
+
+    # print(r.json())
+    json_response = r.json()
+    extracted_text = json_response.get('text', 'Text Not Found')
+
+    return extracted_text
