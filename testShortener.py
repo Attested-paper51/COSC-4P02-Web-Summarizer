ba38a47eba974a6feb01fbcea61ca90c7dea7d90
Amani A <anderson.amani13@gmail.com>
2024-02-08 21:45:31 -0500

    Filled more methods in test file

--- testShortener.py
+++ testShortener.py
@@ -16,5 +16,10 @@ class TestShortenURL():
         response = self.client.get('/')
         self.assertEqual(response.status_code,200)
 
+    def test_shorten_route(self):
+        response = self.client.post('/shorten',data = {'originalURL':'https://medium.com/muthoni-wanyoike/implementing-text-summarization-using-openais-gpt-3-api-dcd6be4f6933'})
+        self.assertEqual(response.status.code, 200)
+        self.assertIn(b'Original URL:',response.data)
+        
 
 
