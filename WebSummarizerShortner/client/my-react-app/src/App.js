b2ca0dadf6f77c1769cdca0e94fa96d6e09c424c
anjali <anjalisabu.jp@gmail.com>
2024-02-09 13:49:42 -0500

    Created LogIn component

--- WebSummarizerShortner/client/my-react-app/src/App.js
+++ WebSummarizerShortner/client/my-react-app/src/App.js
@@ -4,6 +4,7 @@ import Summarizer from './components/Summarizer.js';
 import LandingPage from './components/LandingPage.js';
 import Footer from './components/Footer.js';
 import Hero from './components/Hero.js';
+import LogIn from './components/LogIn.js';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Hero />
       <LandingPage />
+      {/* <LogIn /> */}
       {/* <Footer /> */}
     </div>
   );
