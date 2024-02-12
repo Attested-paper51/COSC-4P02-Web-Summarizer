a1dd6ff5ac9dc096601e2e16bd2b043bd668d6a4
Amani A <anderson.amani13@gmail.com>
2024-02-12 16:24:45 -0500

    Working test cases added

--- testShortener.py
+++ testShortener.py
@@ -1,46 +1,46 @@
 import pytest
+
 from shortenerNoFlask import SimpleURLShortener
 
 
 
-#from flask_testing import TestCase
 
-from shortenerNoAPI import app,db,ShortenedURL
+#def urlShortener():
+#    return SimpleURLShortener
 
-class TestShortenURL():
+def testTableCreated():
+    pass
 
-    def urlShortener():
-        return SimpleURLShortener
+def testlinkShortened():
+    urlShortener = SimpleURLShortener()
+    original_url = "www.testing.com"
+    short_url = urlShortener.shorten_url(original_url)
+    assert short_url is not None
 
-    def testTableCreated():
-        pass
+            
 
-    def testlinkShortened(urlShortener):
-        original_url = "www.testing.com"
-        short_url = urlShortener.shorten_url(original_url)
-        assert short_url is not None
-
+def testLinkRedirected():
+    urlShortener = SimpleURLShortener()
+    original_url = "www.testing.com"
+    short_url = urlShortener.shorten_url(original_url)
+    resolved_url = urlShortener.resolve_url(short_url)
+    assert resolved_url == original_url
         
 
-    def testLinkRedirected(urlShortener):
-        original_url = "www.testing.com"
-        short_url = urlShortener.shorten_url(original_url)
-        resolved_url = urlShortener.resolve_url(short_url)
-        assert resolved_url == original_url
-        
+def testClickCountInitial():
+    urlShortener = SimpleURLShortener()
+    original_url = "www.testingCountIsZero.com"
+    short_url = urlShortener.shorten_url(original_url)
+    initialCount = urlShortener.getClickCount(short_url)
+    assert initialCount == 0
 
-    def testClickCountInitial(urlShortener):
-        original_url = "www.testingCountIsZero.com"
-        short_url = urlShortener.shorten_url(original_url)
-        initialCount = urlShortener.getClickCount(short_url)
-        assert initialCount == 0
-
-    def testClickCountUpdated(urlShortener):
-        original_url = "www.testingCount.com"
-        short_url = urlShortener.shorten_url(original_url)
-        resolved_url = urlShortener.resolve_url(short_url)
-        count = urlShortener.getClickCount(short_url)
-        assert count == 1
+def testClickCountUpdated():
+    urlShortener = SimpleURLShortener()
+    original_url = "www.testingCount.com"
+    short_url = urlShortener.shorten_url(original_url)
+    resolved_url = urlShortener.resolve_url(short_url)
+    count = urlShortener.getClickCount(short_url)
+    assert count == 1
         
     
 
