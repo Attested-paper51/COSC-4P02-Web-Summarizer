51be403cd5c56b095c2a2bfea42c991e04df06ea
Amani A <anderson.amani13@gmail.com>
2024-02-16 15:38:01 -0500

    Committing changes before merge

--- WebSummarizerShortner/client/my-react-app/src/pages/ShortenerPage.js
+++ WebSummarizerShortner/client/my-react-app/src/pages/ShortenerPage.js
@@ -4,7 +4,7 @@ import Header from '../components/Header.js';
 const ShortenerPage = () => {
     return (
         <div>
-            <Header />
+            <Header />  
         </div>
     );
 };
