d104bd7d2248a4f2831d220a5cf496bc82694104
Attested-paper51 <79871733+Attested-paper51@users.noreply.github.com>
2024-02-18 16:31:07 -0500

    Update: updated footer padding. Added extra div.

--- WebSummarizerShortner/client/my-react-app/src/components/Footer.js
+++ WebSummarizerShortner/client/my-react-app/src/components/Footer.js
@@ -5,12 +5,14 @@ import "./css/FooterStyle.css";
 class Footer extends Component {
   render() {
     return (
-      <footer className='footer-container'>
-        <nav className='footer-text'>
-            <h3 className='footer-help'><Link to="/#">Help</Link></h3>
-            <h3 className='footer-feedback'><Link to="/Feedback">Feedback</Link></h3>
-        </nav>
-      </footer>
+      <div className='footer-container'>
+        <footer>
+          <nav className='footer-text'>
+              <h3 className='footer-help'><Link to="/#">Help</Link></h3>
+              <h3 className='footer-feedback'><Link to="/Feedback">Feedback</Link></h3>
+          </nav>
+        </footer>
+      </div>
     );
   };
 }
