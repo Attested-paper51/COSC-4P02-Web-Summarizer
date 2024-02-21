0203ad74e251ca247791742dafccfbe98610e108
Amani A <anderson.amani13@gmail.com>
2024-02-21 13:01:05 -0500

    Commenting and formatting

--- testShortener.py
+++ testShortener.py
@@ -1,9 +1,9 @@
 import pytest
-
+from flask_cors import CORS
 from shortenerNoFlask import SimpleURLShortener
+import time
 
-
-
+#test that the table is created if it is doesn't already exist
 def testTableCreated():
     urlShortener = SimpleURLShortener()
     cursor = urlShortener.conn.cursor()
@@ -12,12 +12,14 @@ def testTableCreated():
     cursor.close()
     assert table, "The table does not exist in the database."
 
+#test that the link is shortened
 def testlinkShortened():
     urlShortener = SimpleURLShortener()
     original_url = "www.testing.com"
     short_url = urlShortener.shorten_url(original_url)
     assert short_url is not None
-   
+
+#test that the link is resolved
 def testLinkRedirected():
     urlShortener = SimpleURLShortener()
     original_url = "www.testing.com"
@@ -25,7 +27,7 @@ def testLinkRedirected():
     resolved_url = urlShortener.resolve_url(short_url)
     assert resolved_url == original_url
         
-
+#test the click count is 0 initially
 def testClickCountInitial():
     urlShortener = SimpleURLShortener()
     original_url = "www.testingCountIsZero.com"
@@ -33,6 +35,7 @@ def testClickCountInitial():
     initialCount = urlShortener.getClickCount(short_url)
     assert initialCount == 0
 
+#test that the click count is updated
 def testClickCountUpdated():
     urlShortener = SimpleURLShortener()
     original_url = "www.testingCount.com"
@@ -46,6 +49,7 @@ def testUniqueURL():
     urlShortener = SimpleURLShortener()
     original_url = "wwww.testingUnique.com"
     short_url_1 = urlShortener.shorten_url(original_url)
+    time.sleep(0.5)
     short_url_2 = urlShortener.shorten_url(original_url)
     assert short_url_1 != short_url_2
 
@@ -55,15 +59,10 @@ def testCustomURLMade():
     urlShortener = SimpleURLShortener()
     originalURL = "www.testingCustom.com"
     short_url = urlShortener.customShorten_url(originalURL,"customTest")
-    assert short_url == "www.testingCustom.com/customTest"
+    assert short_url == "http://127.0.0.1:5000/customTest"
     short_url = urlShortener.customShorten_url(originalURL,"customTest")
     assert short_url == -1
 
-#--obsolete with the above assertion
-#test that there cannot exist 2 of the same custom URLs
-def testNoSameCustomURL():
-    pass
-        
     
 
 
