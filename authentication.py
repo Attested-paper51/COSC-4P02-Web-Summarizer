bb6d95f4102737640e8e518a16d3b803dc3c4464
Amani A <anderson.amani13@gmail.com>
2024-02-29 18:17:33 -0500

    Added git ignore for .env file withe the database password, added user registration capability

--- authentication.py
+++ authentication.py
@@ -1,16 +1,23 @@
 from flask import Flask, request, jsonify
 import psycopg2
+import os
+from dotenv import load_dotenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 class Authentication:
     def __init__(self):
+        load_dotenv()
+        databasePW = os.getenv("DATABASE_PW")
         self.conn = psycopg2.connect(
-            database="database4p02",
-            user="postgres",
-            password="password",
-            host="localhost",
-            port="5432"
+            database="encvegpm",
+            user="encvegpm",
+            password=databasePW,
+            host="drona.db.elephantsql.com",
+            port="5432",
+            sslmode="require"
         )
 
     def registerUser(self,username,password):
@@ -18,20 +25,29 @@ class Authentication:
         cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
         self.conn.commit()
 
+    def checkIfAlreadyRegistered(self,username):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT COUNT(*) FROM users WHERE username = %s", (username,))
+        count = cursor.fetchone()[0]
+        return count > 0
+
     def __del__(self):
         self.conn.close()
 
 
-@app.route('/signup', methods=['POST'])
+@app.route('/register', methods=['POST'])
 def signup():
     data = request.get_json()
     # Extract user data from the request
-    username = data.get('username')
-    password = data.get('password')
+    username = data.get('user')
+    password = data.get('pass')
 
     # Insert the user data into the database
     userMgr = Authentication()
+    if (userMgr.checkIfAlreadyRegistered(username)):
+        return jsonify({'message':'Username is already taken'})
     userMgr.registerUser(username,password)
+    #print("request made")
 
     return jsonify({'message': 'User registered successfully'})
 
