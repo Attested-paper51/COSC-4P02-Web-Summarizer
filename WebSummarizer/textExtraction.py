f2ebb0d8c4ace4d25a28f86fe56ccbb4ada9ca39
theamws <129793438+theamws@users.noreply.github.com>
2024-03-01 16:57:17 -0500

    adding backend to UI, to implement back end

---
+++ WebSummarizer/textExtraction.py
@@ -0,0 +1,23 @@
+import logging
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import time
+
+def extract_text_from_url(url):
+    try:
+        options = Options()
+        options.headless = False
+        logging.getLogger('urllib3.connectionpool').setLevel(logging.ERROR)
+        browser = webdriver.Chrome(options=options)
+        browser.get(url)
+        time.sleep(5)  # Adjust this delay as needed
+        page_source = browser.page_source
+        browser.quit()
+        soup = BeautifulSoup(page_source, 'html.parser')
+        readable_text = soup.get_text(separator=' ', strip=True)
+        return readable_text
+    except Exception as e:
+        logging.error(f"Error: {e}")
+        return "error"
+
