31ead246504a4fef29c3b21df25cf3d62e469da8
anjali <anjalisabu.jp@gmail.com>
2024-03-08 15:24:04 -0500

    Added error messages in user authentication pages - need backend integration

--- WebSummarizerShortner/client/my-react-app/src/components/LogIn.js
+++ WebSummarizerShortner/client/my-react-app/src/components/LogIn.js
@@ -68,8 +68,7 @@ const LogIn = () => {
             onChange={(e) => setEmail(e.target.value)}
             placeholder='Enter email here' 
           />
-          {/* <div className="user-text">Your email</div>
-          <textfield className="textfield"></textfield> */}
+          <div class="email-error">Incorrect Email ID!</div>
         </div>
       
         <div className="password">
@@ -81,6 +80,7 @@ const LogIn = () => {
             onChange={(e) => setPass(e.target.value)}
             placeholder='Enter password here' 
           />
+          <div class="pass-error">Incorrect Password!</div>
         </div>
 
         <Link to="/#">
