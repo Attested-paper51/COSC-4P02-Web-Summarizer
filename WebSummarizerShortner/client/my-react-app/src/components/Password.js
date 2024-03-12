e843ee6763bf3c43fa8ed0bf1bcd81cfda4b630f
Amani A <anderson.amani13@gmail.com>
2024-03-11 20:53:05 -0400

    Included error handling for password resetting, included email corresponding to password reset in the parameter of the link

--- WebSummarizerShortner/client/my-react-app/src/components/Password.js
+++ WebSummarizerShortner/client/my-react-app/src/components/Password.js
@@ -1,17 +1,60 @@
 import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import "./css/PasswordStyle.css";
 
 const Password = () => {
 
   const [pass, setPass] = useState('');
   const [finalPass, confirmPass] = useState('');
+  //const [email, setEmail] = useState('');
 
-  const handleSubmit = (e) => {
-      const Password = {pass}
-      const ConfirmPassword = {finalPass}
-      console.log(Password)
-      console.log(ConfirmPassword)
+  const [passError, setPassError] = useState('');
+  const [finalError, setFinalError] = useState('');
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const email = new URLSearchParams(location.search).get('email') || '';
+  const handleSubmit = async () => {
+    if (pass !== finalPass) {
+      setFinalError('Passwords do not match!');
+      return;
+  }
+
+    const response = await fetch('http://localhost:5001/reset', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email,pass }),
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+      console.log(result);
+      if (result.message === 'Password changed successfully.'){
+        //Navigate to dashboard if password change successful
+        navigate('/Dashboard');
+      }else if (result.message === 'New password requirements not met.') {
+        setPassError(result.message);
+        setPass('');
+        confirmPass('');
+      }
+
+    }else {
+      console.error('Failed to reset.');
+    }
+  }
+
+  const handlePassChange = (e) => {
+    setPassError('');
+    setFinalError('');
+    setPass(e.target.value);
+  }
+
+  const handleConfirmChange = (e) => {
+    setPassError('');
+    setFinalError('');
+    confirmPass(e.target.value);
   }
 
   return (
@@ -36,10 +79,10 @@ const Password = () => {
             type="text" 
             required
             value={pass}
-            onChange={(e) => setPass(e.target.value)}
+            onChange={handlePassChange}
             placeholder='Enter password here' 
           />
-          <div class="pass-error">Password requirements not met.</div>
+          {passError && <div class="pass-error">{passError}</div>}
         </div>
 
         <div className="password">
@@ -48,10 +91,10 @@ const Password = () => {
             type="text" 
             required
             value={finalPass}
-            onChange={(e) => confirmPass(e.target.value)} 
+            onChange={handleConfirmChange} 
             placeholder='Enter password here' 
           />
-          <div class="pass-error">Passwords do not match!</div>
+          {finalError && <div class="pass-error">{finalError}</div>}
         </div>
 
         <button className="reset-btn" onClick={handleSubmit}>
