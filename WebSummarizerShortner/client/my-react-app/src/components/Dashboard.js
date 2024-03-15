6d9ce533a87de8918c3983de2c924afb585f88c8
Amani A <anderson.amani13@gmail.com>
2024-03-14 21:47:22 -0400

    Impovement made on caching logged in credentials

--- WebSummarizerShortner/client/my-react-app/src/components/Dashboard.js
+++ WebSummarizerShortner/client/my-react-app/src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import "./css/DashboardStyle.css";
 //import { Link } from 'react-router-dom';
 import { AuthContext  } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
 
@@ -9,17 +10,30 @@ const Dashboard = () => {
   //persist after refreshing the page.
   const { userEmail } = useContext(AuthContext);
   const [authenticated, setAuthenticated] = useState(false);
+  const storedEmail = localStorage.getItem('email');
+  const navigate = useNavigate();
 
   useEffect (() => {
     const isAuthenticated = localStorage.getItem('authenticated');
-    const userEmail = localStorage.getItem('email');
+    //const userEmail = localStorage.getItem('email');
     setAuthenticated(!!isAuthenticated);
     
 
     console.log('Authentication state is: ',isAuthenticated);
-    console.log('Authenticated email: ',userEmail);
+    //console.log('Authenticated email: ',userEmail);
 
   }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('email');
+    localStorage.removeItem('authenticated');
+    navigate('/');
+  };
+
+  //console.log('Authentication state is: ',isAuthenticated);
+  console.log('AuthContext email: ',userEmail);
+  console.log('Local Storage email: ',storedEmail);
+  
 
   return (
 
@@ -27,13 +41,15 @@ const Dashboard = () => {
         <div className="side-panel">
             <div>Image</div>
             
-            {userEmail && <div>{userEmail}</div>} {/* Change this eventually*/}
+            {storedEmail && <div>{storedEmail}</div>} {/* Change this eventually*/}
             <div className='dashboard-options'>
                 <div className='dash-option'>History</div>
                 <div className='dash-option'>Templates</div>
                 <div className='dash-option'>API Access</div>
                 <div className='dash-option'>Settings</div>
+
             </div>
+            <button onClick={handleLogout}>Logout</button>
         </div>
         <div className="main-panel">
             MAIN PANEL
