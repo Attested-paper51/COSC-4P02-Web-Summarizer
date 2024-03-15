1d864e641b6613b32069515b30205cfc6e0c7140
anjali <anjalisabu.jp@gmail.com>
2024-03-15 00:04:38 -0400

    Dashboard side panel completed

---
+++ WebSummarizerShortner/client/my-react-app/src/components/APIAccess.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import "./css/DashboardStyle.css";
+
+const APIAccess = () => {
+
+  return (
+
+    <div className="wrapper">
+        
+        hello api access
+    
+    </div>
+
+  );
+}
+
+export default APIAccess;
---
+++ WebSummarizerShortner/client/my-react-app/src/components/Settings.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import "./css/DashboardStyle.css";
+
+const Settings = () => {
+
+  return (
+
+    <div className="wrapper">
+        
+        hello settings
+    
+    </div>
+
+  );
+}
+
+export default Settings;
