67f72d68b91a3dbde4eed7b92dbc72c70b732113
Marium <marium.nur2003@gmail.com>
2024-03-16 20:35:48 -0400

    responsiveness and eye icon errors fixed

--- WebSummarizerShortner/client/my-react-app/src/components/SignUp.js
+++ WebSummarizerShortner/client/my-react-app/src/components/SignUp.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import "./css/SignUpStyle.css";
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
 //import { Link } from 'react-router-dom';
 
 const SignUp = () => {
 
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
   const [finalPass, confirmPass] = useState('');
   
   const [emailError, setEmailError] = useState('');
   const [passError, setPassError] = useState('');
+  const [finalError, setFinalError] = useState('');
+
+  const [visible, setVisible] = useState(false);
+  const [visible1, setVisible1] = useState(false);
 
 
   const handleSubmit = async () => {
@@ -55,6 +62,10 @@ const SignUp = () => {
 
   };
 
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+  
   const handleEmailChange = (e) => {
     setEmailError('');
     setEmail(e.target.value);
@@ -62,11 +73,13 @@ const SignUp = () => {
 
   const handlePassChange = (e) => {
     setPassError('');
+    setFinalError('');
     setPass(e.target.value);
   }
 
   const handleConfirmChange = (e) => {
     setPassError('');
+    setFinalError('');
     confirmPass(e.target.value);
   }
 
@@ -75,6 +88,17 @@ const SignUp = () => {
       <div className="form">
         <div className='form-title'>Create an account</div>
         <div className="form-subtitle">Join us to access to Tailored Summaries, Analytics, API Integration and more!</div>
+
+        <div className="name">
+          <label className='name-text'>Enter your name</label> 
+          <input className='textfield'
+            type="text" 
+            required
+            value={name}
+            onChange={handleNameChange}
+            placeholder='Enter name here' 
+          />
+        </div>
 
         <div className="email">
           <label className='email-text'>Enter your email</label> 
@@ -90,25 +114,36 @@ const SignUp = () => {
       
         <div className="password">
           <label className='pass-text'>Enter your password</label> 
-          <input className='textfield'
-            type="text" 
-            required
-            value={pass}
-            onChange={handlePassChange}
-            placeholder='Enter password here' 
-          />
+          <div className='pass-container'>
+            <input className='textfield'
+              type={visible ? "text":"password"}
+              required
+              value={pass}
+              onChange={(e) => setPass(e.target.value)}
+              placeholder='Enter password here' 
+            />
+            <div className='hide-pass1' onClick = {() => setVisible(!visible)}>
+              {visible ? <FaRegEye/> : <FaRegEyeSlash/>}
+            </div>
+          </div>
+          {passError && <div class="pass-error">{passError}</div>}
         </div>
 
         <div className="password">
           <label className='pass-text'>Re-enter password</label> 
-          <input className='textfield'
-            type="text" 
-            required
-            value={finalPass}
-            onChange={handleConfirmChange}
-            placeholder='Enter password here' 
-          />
-          {passError && <div className="pass-error">{passError}</div>}
+          <div className='pass-container'>
+            <input className='textfield'
+                type={visible1 ? "text":"password"}
+                required
+                value={finalPass}
+                onChange={handleConfirmChange} 
+                placeholder='Enter password here' 
+              />
+              <div className='hide-pass2' onClick = {() => setVisible1(!visible1)}>
+                  {visible1 ? <FaRegEye/> : <FaRegEyeSlash/>}
+              </div>
+          </div>
+          {finalError && <div class="pass-error">{finalError}</div>}
         </div>
 
         <button className="signup-btn" onClick={handleSubmit}>
