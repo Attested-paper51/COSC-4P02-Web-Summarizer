a8b0f8f948f515420ac9b46aefce7d6a9bb974a0
Marium <marium.nur2003@gmail.com>
2024-03-17 21:01:22 -0400

    password requirements

--- WebSummarizerShortner/client/my-react-app/src/components/SignUp.js
+++ WebSummarizerShortner/client/my-react-app/src/components/SignUp.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import "./css/SignUpStyle.css";
 import { FaRegEye } from "react-icons/fa";
 import { FaRegEyeSlash } from "react-icons/fa";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuestionCircle, faTimes, faCheck } from '@fortawesome/free-solid-svg-icons';
+import Tooltip from '@mui/material/Tooltip';
 //import { Link } from 'react-router-dom';
 
 const SignUp = () => {
@@ -71,6 +74,58 @@ const SignUp = () => {
     setEmail(e.target.value);
   };
 
+  const valid = ( item, v_icon, inv_icon ) => {
+    let text = document.querySelector(`#${item}`);
+    text.style.opacity = "1";
+
+    let valid_icon = document.querySelector(`#${item} .${v_icon}`);
+    valid_icon.style.opacity = "1";
+
+    let invalid_icon = document.querySelector(`#${item} .${inv_icon}`);
+    invalid_icon.style.opacity = "0";
+  };
+
+  const invalid = ( item, v_icon, inv_icon ) => {
+    let text = document.querySelector(`#${item}`);
+    text.style.opacity = ".5";
+
+    let valid_icon = document.querySelector(`#${item} .${v_icon}`);
+    valid_icon.style.opacity = "0";
+
+    let invalid_icon = document.querySelector(`#${item} .${inv_icon}`);
+    invalid_icon.style.opacity = "1";
+  }
+
+  // const [capital, setCapital] = useState(false);
+  // const [number, setNumber] = useState(false);
+  // const [length, setLength] = useState(false);
+
+  const handleInputChange = (e) => {
+    setPass(e.target.value)
+    const password =e.target.value;
+
+    if (password.match(/[A-Z]/) != null) {
+      valid ('uppercase', 'fa-check', 'fa-times');
+      // setCapital(true)
+    }
+    else {
+      invalid('uppercase', 'fa-check', 'fa-times');
+      // setCapital(false)
+    }
+    if (password.match(/[0-9]/) != null) {
+      valid ('number', 'fa-check', 'fa-times');
+    }
+    else {
+      invalid('number', 'fa-check', 'fa-times');
+    }
+    if (password.length > 7 && password.length<21) {
+      valid ('length', 'fa-check', 'fa-times');
+    }
+    else {
+      invalid('length', 'fa-check', 'fa-times');
+    }
+  };
+
   const handlePassChange = (e) => {
     setPassError('');
     setFinalError('');
@@ -82,6 +137,14 @@ const SignUp = () => {
     setFinalError('');
     confirmPass(e.target.value);
   }
+
+  const [checklist, showChecklist] = useState(true);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", () => {
+      showChecklist(true);
+    });
+  });
 
   return (
     <div className="box">
@@ -113,18 +176,64 @@ const SignUp = () => {
         </div>
       
         <div className="password">
-          <label className='pass-text'>Enter your password</label> 
-          <div className='pass-container'>
-            <input className='textfield'
-              type={visible ? "text":"password"}
-              required
-              value={pass}
-              onChange={(e) => setPass(e.target.value)}
-              placeholder='Enter password here' 
-            />
-            <div className='hide-pass1' onClick = {() => setVisible(!visible)}>
-              {visible ? <FaRegEye/> : <FaRegEyeSlash/>}
+          <div className='pass-req'>
+            <div>
+              <label className='pass-text'>Enter your password</label> 
             </div>
+            <Tooltip className="pass-req-text" title="Password must have 8-20 characters, atleast one uppercase and atleast one number." placement='right' arrow>
+              <div>
+                  <FontAwesomeIcon icon={faQuestionCircle} className='pass-req-icon'/>
+              </div>
+            </Tooltip>
+          </div>
+          <div className='pass-wrapper'>
+            <div className='pass-container' onClick={()=> { showChecklist(true); }}>
+                <input className='textfield'
+                  type={visible ? "text":"password"}
+                  required
+                  value={pass}
+                  onClick={(e)=> { setPass(e.target.value); }}
+                  onChange={handleInputChange}
+                  placeholder='Enter password here' 
+                />
+              <div className='hide-pass1' onClick = {() => setVisible(!visible)}>
+                {visible ? <FaRegEye/> : <FaRegEyeSlash/>}
+              </div>
+            </div>
+
+            {checklist && 
+            <div className='pass-checklist'>
+              <div>
+              <p id='length'>
+                <FontAwesomeIcon className='fa-times icon' icon={faTimes}/>
+                <FontAwesomeIcon className='fa-check icon' icon={faCheck}/>
+                <span>8 to 20 characters long</span>
+              </p>
+              <p id='uppercase'>
+                <FontAwesomeIcon className='fa-times icon' icon={faTimes}/>
+                <FontAwesomeIcon className='fa-check icon' icon={faCheck}/>
+                <span>Atleast 1 uppercase letter</span>
+              </p>
+              <p id='number'>
+                <FontAwesomeIcon className='fa-times icon' icon={faTimes}/>
+                <FontAwesomeIcon className='fa-check icon' icon={faCheck}/>
+                <span>Atleast 1 number</span>
+              </p>
+              </div>
+            </div>
+            } 
+
+            {/* <div className='pass-checklist'>
+              <h3 className='checklist-title'>Password should be</h3>
+              <ul className='checklist'>
+                <li className='list-item'>
+                  8 to 20 characters long
+                </li>
+                <li className='list-item'>At least 1 number</li>
+                <li className='list-item'>At least 1 uppercase letter</li>
+              </ul>
+            </div> */}
+
           </div>
           {passError && <div class="pass-error">{passError}</div>}
         </div>
@@ -136,7 +245,9 @@ const SignUp = () => {
                 type={visible1 ? "text":"password"}
                 required
                 value={finalPass}
-                onChange={handleConfirmChange} 
+                // onChange={handleConfirmChange} 
+                onClick={handleConfirmChange}
+                // onChange={(e) => setPass(e.target.value)}
                 placeholder='Enter password here' 
               />
               <div className='hide-pass2' onClick = {() => setVisible1(!visible1)}>
