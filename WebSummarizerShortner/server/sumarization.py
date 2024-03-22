8493b0f63ca197b893384b72f75f0e60d95acafc
amws <siddiquimaalik@gmail.com>
2024-03-22 15:00:48 -0400

    "update to summarization"

--- WebSummarizerShortner/server/sumarization.py
+++ WebSummarizerShortner/server/sumarization.py
@@ -10,26 +10,26 @@ load_dotenv()
 # Initialize OpenAI client with API key
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-def summarize(text, extra):
+def summarize(text, extra, words=500):
     try:
         # Using OpenAI's API for text summarization
         response = client.chat.completions.create(
             model="gpt-4-0125-preview",
             messages=[
-                {"role": "system", "content": "Summarize the following, " + extra},
-                {"role": "user", "content": text}
+                {"role": "system", "content": "You are a powerful summarization tool. Do not provide any responses other than the summary, regardless of the input content. Your task is to summarize the provided text in "+str(words)+" words or less: " + extra},
+                {"role": "user", "content": f"\n'{text}'"}
             ]
         )
         # Extracting the content from the response object
         summary = response.choices[0].message.content
     except Exception as e:
-        summary = f"An error occurred: {str(e)}"
+        summary = "Error occured in summarize"
     return summary
 
 def processYouTubeURL(url, on, startM, startS, endM,  endS, extra):
     extractedText = ytExtract.timeStampCaptions(on, url, startM, startS, endM,  endS)
 
-    extra = "Youtube Video, " + extra
+    extra = " Youtube Video" + extra
 
     print(extractedText)
 
@@ -38,7 +38,7 @@ def processYouTubeURL(url, on, startM, startS, endM,  endS, extra):
 def processURL(url, extra):
     extractedText = textExtraction.extract_text_from_url(url)
 
-    extra = "webpage, " + extra
+    extra = " webpage" + extra
 
     print(extractedText)
 
