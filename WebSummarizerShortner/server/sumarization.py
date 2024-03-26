11b2377d80a14a74f209841f2e88d7733a420539
amws <siddiquimaalik@gmail.com>
2024-03-26 16:55:19 -0400

    integrated new frontend

--- WebSummarizerShortner/server/sumarization.py
+++ WebSummarizerShortner/server/sumarization.py
@@ -10,13 +10,13 @@ load_dotenv()
 # Initialize OpenAI client with API key
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-def summarize(text, extra, words=100):
+def summarize(text, tone, style, length):
     try:
         response = client.chat.completions.create(
-            model="gpt-4-0125-preview",
+            model="gpt-3.5-turbo-0125",
             messages=[
-                {"role": "system", "content": f"You are a powerful summarization tool. Your task is to summarize the provided text in {words} words or less, {extra}"},
-                {"role": "user", "content": f"\n'{text}'"}
+                {"role": "system", "content": f"You are a powerful summarization tool. Your task is to summarize the provided text with a {tone} tone in {style} form. give me a summary {length} in size"},
+                {"role": "user", "content": text}
             ]
         )
         summary = response.choices[0].message.content
@@ -24,18 +24,16 @@ def summarize(text, extra, words=100):
     except Exception as e:
         return {'summary': None, 'error': f"Error occurred in summarize: {str(e)}"}
 
-def processYouTubeURL(url, on, startM, startS, endM, endS, extra):
+def processYouTubeURL(url, on, startM, startS, endM, endS, tone, style, length):
     try:
-        extractedText = ytExtract.timeStampCaptions(on, url, startM, startS, endM, endS)
-        extra = " Youtube Video" + extra
-        return summarize(extractedText, extra)
+        extractedText = f"YouTube video({url}): " + ytExtract.timeStampCaptions(on, url, startM, startS, endM, endS)
+        return summarize(extractedText, tone, style, length)
     except Exception as e:
         return {'summary': None, 'error': f"Error in processing YouTube URL: {str(e)}"}
 
-def processURL(url, extra):
+def processURL(url, tone, style, length):
     try:
-        extractedText = textExtraction.extract_text_from_url(url)
-        extra = " webpage" + extra
-        return summarize(extractedText, extra)
+        extractedText = f"Webpage ({url}): " + textExtraction.extract_text_from_url(url)
+        return summarize(extractedText, tone, style, length)
     except Exception as e:
         return {'summary': None, 'error': f"Error in processing URL: {str(e)}"}
