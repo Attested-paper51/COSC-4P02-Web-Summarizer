b60b3797c183c5f2d67b195c2cafcdff1d86ca5e
Amani A <anderson.amani13@gmail.com>
2024-03-29 18:06:00 -0400

    Added backend and front end integration to have the user's actual username appear in URL shortener custom link

--- WebSummarizerShortner/client/my-react-app/src/components/URLShortener.js
+++ WebSummarizerShortner/client/my-react-app/src/components/URLShortener.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./css/URLShortenerStyle.css";
 import { Link } from 'react-router-dom';
 import { useTheme } from './ThemeContext.js'
@@ -7,7 +7,7 @@ const URLShortener = () => {
 
     const { darkMode } = useTheme();
     const [isPremium, setPremium] = useState(true);
-    const [username, setUsername] = useState('testusername');
+    const [username, setUsername] = useState('');
 
     const [URL, setURL] = useState('');
     const [shortened, setShortURL] = useState('');
@@ -24,7 +24,32 @@ const URLShortener = () => {
 
     const email = localStorage.getItem('email');
     
-
+    useEffect(() => {
+        const fetchUsername = async () => {
+            try {
+                const response = await fetch('http://localhost:5001/getusername', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ email }),
+                });
+    
+                if (response.ok) {
+                    const result = await response.json();
+                    setUsername(result.message);
+                } else {
+                    console.error('Failed to fetch username');
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+        
+        if (email) {
+            fetchUsername();
+        }
+    }, [email]);
     // const handleSubmit = (e) => {
     //     const url = {URL} 
     //     console.log(url)
