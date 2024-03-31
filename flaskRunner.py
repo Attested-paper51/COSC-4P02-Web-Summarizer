abc0cdefcb4f1373a1bddf924308baa1d5365c10
Amani A <anderson.amani13@gmail.com>
2024-03-31 17:25:53 -0400

    Deleted redundant files

--- flaskRunner.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-if __name__ == '__main__':
-    file1 = subprocess.Popen(['python','authentication.py'])
-    file2 = subprocess.Popen(['python','shortenerFlask.py'])
-
-    file1.wait()
-    file2.wait()
