3672db4e7e451c20175567ff1db6c135b7e92ebe
Marium <marium.nur2003@gmail.com>
2024-04-25 00:42:07 -0400

    ui testing

--- WebSummarizerShortner/client/my-react-app/src/components/Footer.js
+++ WebSummarizerShortner/client/my-react-app/src/components/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
     <div className='footer'>
       <footer className={`footer-container ${darkMode ? 'footer-dark' : 'footer-light'}`}>
         <nav className='footer-text'>
-            <h3 className='footer-help'><Link to="/#">Help</Link></h3>
+            {/* <h3 className='footer-help'><Link to="/#">Help</Link></h3> */}
             <h3 className='footer-feedback'><Link to="/Feedback">Feedback</Link></h3>
         </nav>
       </footer>
